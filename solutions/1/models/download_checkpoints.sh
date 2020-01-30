1c24005559e498dc239fbcf1ca67bdde7f7da877
Lars Andersen Bratholm <larsbratholm@gmail.com>
2020-01-30 16:39:27 +0000

    Added checkpoint download script

---
+++ solutions/1/models/download_checkpoints.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Get location of script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Download team_1_checkpoints_part_{1,2}.tar.gz
+wget https://osf.io/89yht/download
+wget https://osf.io/46xwf/download
+
+# Unpack
+tar xf team_1_checkpoints_part_{1,2}.tar
+
+# Delete archive
+rm team_1_checkpoints_part_{1,2}.tar
