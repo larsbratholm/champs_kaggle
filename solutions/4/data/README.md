06e5b9c58204dfe9b5bcb97e4fde686facac272c
Lars Andersen Bratholm <larsbratholm@gmail.com>
2020-02-03 16:52:31 +0000

    Added link to preprocessed data

---
+++ solutions/4/data/README.md
@@ -0,0 +1,2 @@
+# data
+Pre-processed data can be downloaded by running the [download_preprocessed_data.sh](../download_preprocessed_data.sh) script.
---
+++ solutions/4/download_preprocessed_data.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Get location of script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Go to data folder
+cd $DIR/data
+
+# Download team_4_preprocessed_data.tar.gz
+wget https://osf.io/ntdhf/download
+
+# Unpack
+tar -zxf team_4_preprocessed_data.tar.gz
+
+# Deleted unneeded files
+rm team_4_preprocessed_data.tar.gz
