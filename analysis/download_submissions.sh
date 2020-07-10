3ae8fd94cbf652f00ac5905728003690716e9126
larsbratholm <larsbratholm@gmail.com>
2020-07-10 13:10:35 +0100

    Updated submission download script as files were missing in previous upload, and added output of plot script

--- analysis/download_submissions.sh
+++ analysis/download_submissions.sh
@@ -7,16 +7,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/data
 
 # Download submissions_part_{1..4}.tar.gz
-wget -O submissions_part_1.tar.gz https://osf.io/9tqs2/download
-wget -O submissions_part_2.tar.gz https://osf.io/tb5ev/download
-wget -O submissions_part_3.tar.gz https://osf.io/4q2tg/download
-wget -O submissions_part_4.tar.gz https://osf.io/e5tbu/download
+wget -O submissions_part_1 https://osf.io/zk2dv/download
+wget -O submissions_part_2 https://osf.io/uwn5x/download
+wget -O submissions_part_3 https://osf.io/vbn7s/download
 
 # Unpack
-tar zxf submissions_part_1.tar.gz
-tar zxf submissions_part_2.tar.gz
-tar zxf submissions_part_3.tar.gz
-tar zxf submissions_part_4.tar.gz
+cat submissions_part_1 submissions_part_2 submissions_part_3 > submissions.tgz
+tar zxf submissions.tgz
 
 # Deleted unneeded files
-rm submissions_part_{1..4}.tar.gz
+rm submissions_part_{1..3}
+rm submissions.tgz
